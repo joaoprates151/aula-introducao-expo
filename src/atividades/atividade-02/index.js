70e7168d7ef7f780d455f341a4d988de4cec7eee
joaoprates151 <joaoprates151@gmail.com>
2025-03-19 21:23:46 -0300

    Mudança

--- src/atividades/atividade-02/index.js
+++ src/atividades/atividade-02/index.js
@@ -1,4 +1,4 @@
-import {View, Text, Image, Pressable} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import styles from './styles';
 import { useState } from 'react';
 
@@ -11,31 +11,17 @@ import camisapreta from '../../../assets/camisa_preta.png'
 import camisaazul from '../../../assets/camiseta_azul.png'
 import camisavermelha from '../../../assets/camiseta_vermelha.png'
 
+
 function Atividade02()
 {
     return(
         <>
             <View style={styles.container}>
-                <Mensagem titulo="Camisa Branca" subtitulo="Camisa Branca Normal" preco="R$39,90"/>
-                <Image style={styles.imagem} source={camisabranca}></Image>
+                <Mensagem titulo="Camisa Branca" subtitulo="Camisa Branca Normal" preco="R$39,90" camisa = {camisabranca}/>
+                <Mensagem titulo="Camisa Preta" subtitulo="Camisa Preta Normal" preco="R$39,90" camisa = {camisapreta}/>
+                <Mensagem titulo="Camisa Azul" subtitulo="Camisa Azul Normal" preco="R$39,90" camisa = {camisaazul}/>
+                <Mensagem titulo="Camisa Vermelha" subtitulo="Camisa Vermelha Normal" preco="R$39,90" camisa = {camisavermelha}/>
             </View>
-
-            <View style={styles.container}>
-                <Mensagem titulo="Camisa Preta" subtitulo="Camisa Preta Normal" preco="R$39,90"/>
-                <Image style={styles.imagem} source={camisapreta}></Image>
-            </View>
-
-            <View style={styles.container}>
-                <Mensagem titulo="Camisa Azul" subtitulo="Camisa Azul Normal" preco="R$39,90"/>
-                <Image style={styles.imagem} source={camisaazul}></Image>
-            </View>
-
-            <View style={styles.container}>
-                <Mensagem titulo="Camisa Vermelha" subtitulo="Camisa Vermelha Normal" preco="R$39,90"/>
-                <Image style={styles.imagem} source={camisavermelha}></Image>
-            </View>
-
-       
         </>
         
     )
--- src/atividades/atividade-02/mensagem/index.js
+++ src/atividades/atividade-02/mensagem/index.js
@@ -1,13 +1,18 @@
-import {View, Text} from 'react-native'
+import {View, Text, Image} from 'react-native'
 import styles from './styles'
+
+
 
 function Mensagem(props)
 {
     return(
         <View style={styles.container}>
+            <div>
             <Text style= {styles.titulo}>{props.titulo}</Text>
             <Text style={styles.texto}>{props.subtitulo}</Text>
             <Text style={styles.texto}>{props.preco}</Text>
+            </div>
+            <Image style={styles.imagem} source={props.camisa}></Image>
         </View>
     )
 }
