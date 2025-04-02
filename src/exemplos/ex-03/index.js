bb7cac9e27b169018e4cbea0d8d930722c70f498
joaoprates151 <joaoprates151@gmail.com>
2025-04-02 20:39:58 -0300

    Atualizar

--- src/exemplos/ex-03/index.js
+++ src/exemplos/ex-03/index.js
@@ -1,20 +1,44 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Alert, TouchableOpacity} from 'react-native';
 import styles from './styles';
+
+import {useState} from 'react'
 
 
 function Exemplo3()
 {
+
+    const [num, setNum] = useState(0)
+
+    function handleIncrementar()
+    {
+        setNum(num + 1)
+    }
     return(
         <View style={styles.container}>
             <Text style= {styles.titulo}>Exemplo3</Text>
             <Text style= {styles.texto}>Variáveis vs State</Text>
-            <Button
-                 onPress={() => {}}
-                 title="Alerta"
-                 color="deepskyblue"
-                 accessibilityLabel="Mensagem de alerta"
-                 
-            />
+            <View style= {styles.botao_alert}>
+                <Button
+                
+                    onPress={() => {Alert.alert('Mensagem', 'Conteúdo',[
+                        {
+                            text: 'Cancel',
+                            onPress: () => alert('Cancel Pressed'),
+                            style: 'cancel',
+                        },
+                    ])}}
+                    title="Alerta"
+                    color="#14870c"
+                    accessibilityLabel="Mensagem de alerta"
+                    
+                />
+            </View>
+
+            <Text style= {styles.valor}>{num}</Text>
+
+            <TouchableOpacity style= {styles.botao_contar} onPress={handleIncrementar}>
+                <Text style= {styles.titulo_btn}>Adicionar +1</Text>
+            </TouchableOpacity>
         </View>
     )
 }
--- src/exemplos/ex-03/styles.js
+++ src/exemplos/ex-03/styles.js
@@ -1,3 +1,4 @@
+import { Children } from 'react';
 import {StyleSheet, TextComponent} from 'react-native'
 import {RFPercentage} from 'react-native-responsive-fontsize';
 
@@ -35,10 +36,33 @@ const styles = StyleSheet.create
         color: 'deepskyblue',
     },
 
-    botao:
+    botao_alert:
     {
-        marginVertical: RFPercentage(1.5),
-        width: '80%'
+        margin: RFPercentage(1.5), 
+        width: '100%',
+    },
+
+    valor:{
+        fontSize: RFPercentage(3),
+        marginBottom: RFPercentage(1.5),
+        color: '#ff8c00',
+        marginTop: '10%'
+    },
+
+    botao_contar:
+    {
+        backgroundColor: 'red',
+        width: '50%',
+        height: RFPercentage(4),
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    titulo_btn:
+    {
+        fontSize: RFPercentage(2),
+        color: '#fff'
     }
 })
 
