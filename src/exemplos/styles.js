d6741e504eb225432be1f6b2824e3c3697ccba4e
joaoprates151 <joaoprates151@gmail.com>
2025-06-10 20:40:27 -0300

    styles

--- src/exemplos/styles.js
+++ src/exemplos/styles.js
@@ -3,20 +3,26 @@ import {RFValue} from 'react-native-responsive-fontsize'
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center'
     },
     titulo: 
     {
         fontSize: RFValue(20),
         fontWeight: 'bold',
-        color: 'deepskyblue'
+        color: 'deepskyblue',
+        marginTop: RFValue(10),
+        marginBottom: RFValue(20)
     },
     botao:
     {
+        alignItems: 'center',
         borderWidth: RFValue(1),
         borderColor: 'deepskyblue',
         borderRadius: RFValue(2),
-        padding: RFValue(1),
-        width: '80%'
+        borderRadius: RFValue(10),
+        padding: RFValue(5),
+        width: '80%',
+        marginBlock: RFValue(10)
     }
 })
 
